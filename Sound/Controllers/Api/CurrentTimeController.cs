c403b25e900cf425046f2e2e1ee7ebd13fb14a6a
Balasubramanian <sbalasubramanian96@gmail.com>
2018-07-25 01:04:23 +0530

    Test Commit

---
+++ Sound/Controllers/Api/CurrentTimeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sound.Controllers.Api
+{
+    public class CurrentTimeController : ApiController
+    {
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("h:mm:ss tt");
+        }
+    }
+}
