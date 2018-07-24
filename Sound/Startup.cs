cbbda02d9a6f6cbb3282f032ef8a9b3d73143af1
Balasubramanian <sbalasubramanian96@gmail.com>
2018-07-24 20:31:16 +0530

    Add project files.

---
+++ Sound/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sound.Startup))]
+namespace Sound
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
